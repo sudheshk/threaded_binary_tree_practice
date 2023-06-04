71e2769bcd0875f93ba5aa5e4c1f4eb605c583b3
sudheshk <sudheshk@yahoo.com>
2023-06-04 15:56:58 -0700

    directing inserting values and removing values

--- main.cpp
+++ main.cpp
@@ -5,12 +5,26 @@
 #include <iostream>
 #define MAX_VALUE 65536
 using namespace std;
-#include "menu_options.cpp"
-//#include "tbt.cpp"
+//#include "menu_options.cpp"
+#include "threaded_binary_tree.cpp"
 
 /* Main Contains Menu */
 
 int main() {
-  menu_options menu_options;
+  // menu_options menu_options;
+  ThreadedBinarySearchTree tbst;
+  tbst.insert(1);
+  tbst.insert(2);
+  tbst.insert(3);
+  tbst.insert(4);
+  tbst.insert(5);
+  /*  Display tree  */
+  cout << "\nTree = ";
+  tbst.printTree();
+  tbst.Delete(3);
+  /*  Display tree  */
+  cout << "\n After removal, Tree = ";
+  tbst.printTree();
+
   return 0;
 }
